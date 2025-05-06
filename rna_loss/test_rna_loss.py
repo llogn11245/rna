4549fb802368ef3eafb3915fea85621cea35246f
Long <longgg11245@gmail.com>
2025-05-06 21:37:15 +0700

    update code loss

--- rna_loss/test_rna_loss.py
+++ rna_loss/test_rna_loss.py
@@ -2,4 +2,4 @@ try:
     from warp_rna import rna_loss
     print("RNA_Loss module imported successfully.")
 except:
-    print("RNA_Loss module not found. Please ensure it is in the same directory as this script.")
+    print("RNA_Loss module not found.")
